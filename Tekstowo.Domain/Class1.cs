bdc893dee90eccffe12d762ba15738882ce50b42
Dominik <domzur1025@gmail.com>
2019-01-19 21:37:49 +0100

    Stworzenie projektu
    Stworzenie projektu Domain, WebUI oraz UnitTest oraz instalacja pakietów NuGet

---
+++ Tekstowo.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Tekstowo.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tekstowo.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tekstowo.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tekstowo.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e3e8da8e-c417-4806-be3a-4e4e48af8629")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
